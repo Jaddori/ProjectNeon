0d50f88b3d07e7f9fa6a1cd78cd95cfcf221cd93
Jaddori <ohlsson.niclas@gmail.com>
2018-07-07 20:27:44 +0200

    dev - Working on menus.

---
+++ Game/ProjectNeon/Assets/Scripts/Menu/Lobby.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lobby : MonoBehaviour
+{
+	public Text[] playerLabels;
+	private string[] playerNames;
+
+	public void UpdatePlayerLabels()
+	{
+		if( playerNames == null )
+		{
+			playerNames = new string[] { "Bojangles", string.Empty, string.Empty, string.Empty };
+		}
+
+		for( int i = 0; i < playerLabels.Length; i++ )
+		{
+			playerLabels[i].text = playerNames[i];
+		}
+	}
+}
---
+++ Game/ProjectNeon/Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+	public Transform mainMenu;
+	public Transform lobby;
+	public Transform joinMenu;
+
+	private Transform _currentMenu;
+
+	private void Awake()
+	{
+		_currentMenu = mainMenu;
+	}
+
+	public void GotoMainMenu()
+	{
+		_currentMenu.gameObject.SetActive( false );
+		mainMenu.gameObject.SetActive( true );
+
+		_currentMenu = mainMenu;
+	}
+
+	public void GotoLobby()
+	{
+		_currentMenu.gameObject.SetActive( false );
+
+		var lobbyScript = lobby.gameObject.GetComponent<Lobby>();
+		lobbyScript.UpdatePlayerLabels();
+
+		lobby.gameObject.SetActive( true );
+		_currentMenu = lobby;
+	}
+
+	public void GotoJoinMenu()
+	{
+		_currentMenu.gameObject.SetActive( false );
+
+		joinMenu.gameObject.SetActive( true );
+
+		_currentMenu = joinMenu;
+	}
+}
