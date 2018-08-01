bb4c209cf7529ad91d9baa20cdaddaf43701aff8
Jaddori <ohlsson.niclas@gmail.com>
2018-08-01 18:04:20 +0200

    dev - Working on lobby scene to select starting color.

---
+++ Game/ProjectNeon/Assets/Scripts/Lobby/ReadyTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ReadyTrigger : MonoBehaviour
+{
+	public int Color;
+
+	private NetManager _netManager;
+	private GameObject _player;
+
+	public void Awake()
+	{
+		var netManagerObject = GameObject.FindGameObjectWithTag( "NetManager" );
+		_netManager = netManagerObject.GetComponent<NetManager>();
+	}
+
+	public void Update()
+	{
+		if( _player != null )
+		{
+			if( Input.GetKey( KeyCode.Space ) )
+			{
+				_netManager.ServerChangeScene( "Sandbox" );
+			}
+		}
+	}
+
+	public void OnTriggerEnter( Collider collider )
+	{
+		if( _player == null )
+		{
+			_player = collider.gameObject;
+		}
+	}
+
+	public void OnTriggerExit( Collider collider )
+	{
+		if( collider.gameObject == _player )
+			_player = null;
+	}
+}
